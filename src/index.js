e05ebe48affb9fdee53302494c365478242d6282
Omar <ogspain@gmail.com>
2021-01-20 18:08:00 -0400

    =Instalados los estilos de UniUITheme

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import UniUiTheme from './uiTheme'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <UniUiTheme>
     <App />
-  </React.StrictMode>,
+  </UniUiTheme>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
